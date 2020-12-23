48526aaa6942c02e48252cd1406a93a237145205
BerniceYeow <44364177+BerniceYeow@users.noreply.github.com>
2020-12-23 22:59:29 +0800

    Add files via upload
    upddate top2vec code leave it as default setting

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def main():
 
         #INITIALIZE THE TOP2VEC MODEL AND FIT THE TEXT
         #model.build_vocab(df_list, update=False)
-        model = Top2Vec(documents=documents, speed="learn", workers=10)
+        model = Top2Vec(documents)
         
         topic_sizes, topic_nums = model.get_topic_sizes()
         for topic in topic_nums:
